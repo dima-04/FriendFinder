7919b2cfbb7832bf49be1e921f9e2b965188f294
Dima <dima.dibb1@gmail.com>
2019-11-22 23:07:21 -0800

    update

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,16 +1,16 @@
 var friends = [
     {
-      name: "zeina",
+      name: "Zina",
       photo: "https://imagevars.gulfnews.com/2018/2/1/1_16a08485935.2166617_305494117_16a08485935_large.jpg",
       scores: [1,5,3,6,7,9,8,8,8,5]
     },
     {
-        name: "hala",
+        name: "Hala",
         photo: "https://upload.wikimedia.org/wikipedia/commons/b/ba/Haifa_Wahbe.jpg",
         scores: [3,6,7,9,8,6,3,2,1,4]
     },
     {
-        name: "rola",
+        name: "Rola",
         photo: "https://i.guim.co.uk/img/static/sys-images/BOOKS/Pix/pictures/2011/11/16/1321447713707/Marilyn-Monroe-007.jpg?width=300&quality=85&auto=format&fit=max&s=c890c8de39188542e85e0bcf5484b134",
         scores: [1,2,5,7,7,6,5,4,4,4]
     }
